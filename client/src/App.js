172d3ff7958d1869c895ed146050f48f46eebb26
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-05-27 22:25:22 -0500

    starting adding some sendgrid

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import Contact from './pages/Contact';
 import Portfolio from './pages/Portfolio';
 import Cards from './components/Cards';
 import { Navbar, Nav } from 'react-bootstrap';
+import ContactShakhor from './pages/ContactShakhor';
 
 
 function App() {
@@ -46,7 +47,7 @@ function App() {
           <Route path='/about' component={About} />
           <Route path='/portfolio' component={Portfolio} />
           <Route path='/resume' component={Resume} />
-          <Route path='/contact' component={Contact} />
+          <Route path='/contact' component={ContactShakhor} />
           <Route path='/cards' component={Cards} />
         </Switch>
         <Footer />
--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import ContactPage from './ContactPage'
 
 function Contact() {
     return (
         <div>
-            Hit
+            <ContactPage />
         </div>
     )
 }
