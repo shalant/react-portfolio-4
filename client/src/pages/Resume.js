bf970a18d1b691fc1ae89f2efb9c213cd18cf2f6
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-06-07 13:13:17 -0500

    fixed headshot in the about page

--- client/src/pages/Resume.js
+++ client/src/pages/Resume.js
@@ -6,7 +6,7 @@ function Resume() {
     return (
         <div className='resume'>
             <a href='https://docs.google.com/document/d/1G1z3cVN6IjHW4M2zVytsoE6g_-fWETb0qTSSZqI05Tk/edit?usp=sharing'>Resume</a>
-            <img src={resume} className='resume'></img>
+            <img src={resume} alt='resume' className='resume'></img>
         </div>
     )
 }
